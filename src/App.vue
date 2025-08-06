0666842ecf18caee572437a342bd474ebd91058f
jakeg96 <jakepgreen6@gmail.com>
2025-08-06 12:31:58 +1000

    UI fixes

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,23 @@ import { Icon } from '@iconify/vue'
 <template>
   <body class="flex flex-col justify-center items-center md:h-screen text-white p-10">
     <div class="text-8xl text-center">Jake Green</div>
-    <div class="my-6 flex flex-col justify-center gap-4">
-        <a href="https://github.com/jakeg96" target="_blank" class="flex items-center hover:text-blue-400"
+    <div class="text-3xl text-center">Frontend Developer</div>
+    <div class="my-6 flex justify-center gap-5">
+        <a href="https://github.com/jakeg96" target="_blank" class="flex items-center text-blue-400 hover:text-blue-300"
           ><Icon icon="simple-icons:github" class="size-6 mr-2" />Github</a
         >
-        <a href="/jakegreen2025.pdf#toolbar=0" target="_blank" class="flex items-center hover:text-blue-400"
+        <a href="/jakegreen2025.pdf#toolbar=0" target="_blank" class="flex items-center text-blue-400 hover:text-blue-300"
           ><Icon icon="qlementine-icons:resume-16" class="size-6 mr-2" />Resume</a
         >
     </div>
-    <div class="text-5xl mb-4">Projects</div>
+    <div class="text-5xl my-4">Projects</div>
     <div class="flex flex-col md:flex-row justify-center gap-5">
       <div class="flex flex-col items-center justify-center md:w-3/12">
         <div class="font-bold text-3xl">SkyTrack</div>
-        <a href="https://skytrack.jakegreen.dev" target="_blank" class=" hover:text-blue-400"
+        <a href="https://skytrack.jakegreen.dev" target="_blank" class="text-blue-400 hover:text-blue-300"
           >skytrack.jakegreen.dev</a
         >
+        
         <div>Stack</div>
         <div class="flex items-center gap-2">
           <Icon icon="simple-icons:vuedotjs" class="size-8" />
@@ -27,10 +29,11 @@ import { Icon } from '@iconify/vue'
           <Icon icon="simple-icons:tailwindcss" class="size-8" />
         </div>
         <div class="mt-2">Tools</div>
-        <div class="flex items-center">
+        <div class="flex items-center gap-2">
           
             <Icon icon="simple-icons:vite" class="size-8" />
           <Icon icon="simple-icons:git" class="size-8" />
+          <Icon icon="simple-icons:github" class="size-8" />
           <Icon icon="simple-icons:vercel" class="size-8" />
         </div>
         <div class="flex text-wrap text-center mt-2">
@@ -38,12 +41,17 @@ import { Icon } from '@iconify/vue'
           calls, Pinia for state management, and displays real-time AviationStack data in a clean
           UI. Hosted on Vercel.
         </div>
+        <a href="https://github.com/jakeg96/skytrack" target="_blank" class="text-blue-400 hover:text-blue-300"
+          >github.com/jakeg96/skytrack</a
+        >
       </div>
+      
       <div class="flex flex-col items-center md:w-3/12 ">
         <div class="font-bold text-3xl">PC</div>
-        <a href="https://pc.jakegreen.dev" target="_blank" class="hover:text-blue-400"
+        <a href="https://pc.jakegreen.dev" target="_blank" class="text-blue-400 hover:text-blue-300"
           >pc.jakegreen.dev</a
         >
+        
         <div>Stack</div>
         <div class="flex items-center gap-2">
           <Icon icon="simple-icons:vuedotjs" class="size-8" />
@@ -51,14 +59,18 @@ import { Icon } from '@iconify/vue'
           <Icon icon="simple-icons:tailwindcss" class="size-8" />
         </div>
         <div class="mt-2">Tools</div>
-        <div class="flex items-center">
+        <div class="flex items-center gap-2">
             <Icon icon="simple-icons:vite" class="size-8" />
           <Icon icon="simple-icons:git" class="size-8" />
+          <Icon icon="simple-icons:github" class="size-8" />
           <Icon icon="simple-icons:vercel" class="size-8" />
         </div>
         <div class="flex text-wrap text-center mt-2">
           A personal project loosely based on the Apple Lisa PC.
         </div>
+        <a href="https://github.com/jakeg96/pc" target="_blank" class="text-blue-400 hover:text-blue-300"
+          >github.com/jakeg96/pc</a
+        >
       </div>
     </div>
   </body>
